e0b2e37518058270236a01cf219806bb2f9af4ac
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-08-12 12:49:40 -0400

    added modal to medications component

--- components/Medications/index.js
+++ components/Medications/index.js
@@ -1,52 +1,70 @@
-import { Table, Button } from 'antd'
+import { useState } from 'react'
+import { Table, Modal, Button } from 'antd'
 import './style.scss'
 
-const dataSource = [
-  {
-    key: '1',
-    name: 'Lysergic acid diethylamide',
-    dose: '25 mg',
-    lastRefill: '08/15/2020',
-  },
-  {
-    key: '2',
-    name: 'Methyl​enedioxy​methamphetamine',
-    dose: '50 mg',
-    lastRefill: '06/12/2020',
-  },
-  {
-    key: '3',
-    name: 'Psilocybe mexicana',
-    dose: '150 mg',
-    lastRefill: '05/14/2020',
-  },
-] 
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Dose',
-    dataIndex: 'dose',
-    key: 'dose',
-  },
-  {
-    title: 'Last Refill',
-    dataIndex: 'lastRefill',
-    key: 'lastRefill',
-  },
-  {
-    title: 'Action',
-    dataIndex: 'action',
-    render: (text) => 
-      <Button className="refill-btn" block type="primary">Refill {text}</Button>
-  },
-] 
-
 const Medications = () => {
+  const [visible, setVisible] = useState(false)
+  const showModal = (e) => setVisible(true)
+  const handleOk = (e) => {
+    console.log(e)
+    setVisible(false)
+  }
+  const handleCancel = (e) => {
+    console.log(e)
+    setVisible(false)
+  }
+
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Lysergic acid diethylamide',
+      dose: '25 mg',
+      lastRefill: '08/15/2020',
+    },
+    {
+      key: '2',
+      name: 'Methyl​enedioxy​methamphetamine',
+      dose: '50 mg',
+      lastRefill: '06/12/2020',
+    },
+    {
+      key: '3',
+      name: 'Psilocybe mexicana',
+      dose: '150 mg',
+      lastRefill: '05/14/2020',
+    },
+  ]
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Dose',
+      dataIndex: 'dose',
+      key: 'dose',
+    },
+    {
+      title: 'Last Refill',
+      dataIndex: 'lastRefill',
+      key: 'lastRefill',
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      render: (text) =>
+        <Button
+          className="refill-btn"
+          onClick={showModal}
+          block
+          type="primary">
+          Refill {text}
+        </Button>
+    },
+  ] 
+
   return (
     <>
       <Table
@@ -54,6 +72,16 @@ const Medications = () => {
         dataSource={dataSource}
         bordered
       />
+      <Modal
+        title="Refill Prescription"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </>
   )
 }
