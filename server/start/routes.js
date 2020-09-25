8fa595a9d4d2f2093839c9184bf0a21226ed9474
Matthew Jaikaran <mattjaikaran@gmail.com>
2020-09-25 15:52:18 -0400

    added nodejs api, started auth

---
+++ server/start/routes.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.get('/', () => {
+  return { greeting: 'Hello to Patient Portal API' }
+})
+
+
+Route.post('login', 'UserController.login').middleware('guest')
+Route.get('users/:id', 'UserController.show').middleware('auth')
+
+Route.get('/profile', 'UserController.profile').middleware('auth')
+
